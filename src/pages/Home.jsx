ef47330e111ba6876976ce7c1044e322fac27f11
Cris <ghinghiloschicristina@gmail.com>
2023-08-29 10:36:16 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,20 +10,20 @@ export const Home = () => {
     }
 
     return (
-        <section className="h-[calc(100vh-68px)] flex flex-col md:flex-row justify-center items-center pb-8 lg:pb-20 text-center lg:text-left">
+        <section className="h-[calc(100vh-104px)] flex flex-col md:flex-row justify-center items-center pb-8 lg:pb-20 text-center lg:text-left">
 
             {/* text and button */}
             <div className="md:order-2 flex-grow flex justify-center items-center md:w-1/2">
                 <div className="text-center md:text-left w-full">
-                    <h2 className="mt-8 text-6xl md:text-8xl font-light uppercase">
-                        LET GO
+                    <h2 className="mt-8 text-6xl md:text-8xl font-letgo">
+                        Let go
                     </h2>
-                    <h3 className="text-xl md:text-3xl font-extralight">
+                    <h3 className="text-lg md:text-2xl font-extralight">
                         Unleash your energy centers
                     </h3>
                     <button
                         onClick={handleGetStarted}
-                        className="mt-10 border-2 px-6 py-3 uppercase rounded hover:bg-white hover:text-rose-500 dark:hover:text-slate-700 text-white tracking-wider cursor-pointer hover:scale-105 duration-300 ">
+                        className="mt-6 lg:mt-8 border-2 px-4 py-2 text-sm lg:text-base uppercase rounded hover:bg-white hover:text-rose-500 dark:hover:text-slate-700 text-white tracking-wider cursor-pointer hover:scale-105 duration-300 ">
                         Get Started
                     </button>
                 </div>
