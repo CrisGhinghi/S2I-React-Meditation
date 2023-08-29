1f10c8bc41bc5dca90d6d587768354f9146aa74c
Cris <ghinghiloschicristina@gmail.com>
2023-08-29 11:48:11 +0200

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+export const Footer = () => {
+    return (
+      <section className="text-center py-4">
+          <p className="">
+              © 2023 CRIS. All rights reserved.
+          </p>
+      </section>
+    )
+  }
