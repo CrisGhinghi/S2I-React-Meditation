3e9692be4600e2d0874b54fac48e0a2f9557ecda
Cris <ghinghiloschicristina@gmail.com>
2023-08-29 10:36:20 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
+import logo from "../assets/img/logo.png";
 import { MdNightsStay, MdWbSunny } from 'react-icons/md'
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 
 export const Header = ({ darkMode, setDarkMode }) => {
@@ -7,25 +9,30 @@ export const Header = ({ darkMode, setDarkMode }) => {
     };
 
     return (
-      <header className='bg-rose-700 dark:bg-gray-950 text-white'>
-          <nav className='flex justify-between items-center p-5'>
-              <h1 className='text-lg'>LET GO</h1>
-              <button 
-                  onClick={handleDarkModeToggle} 
-                  aria-label="Toggle dark mode" 
-                  className='focus:outline-none'
-              >
-                  {
-                      darkMode ? (
-                          <MdWbSunny className='text-2xl cursor-pointer' />
-                      ) : (
-                          <MdNightsStay className='text-2xl cursor-pointer' />
-                      )
-                  }
-              </button>
-          </nav>
-      </header>
-  )
+        <header className='bg-rose-700 dark:bg-gray-950 text-white'>
+            <nav className='flex justify-between items-center p-5'>
+                <div className='flex items-center'>
+                    <img src={logo} alt="logo" className='mr-1 h-12 w-12 align-middle mb-4' />
+                    <Link to="/"> {/* Aggiunto il componente Link */}
+                        <h1 className='text-3xl align-middle cursor-pointer font-letgo'>Let go</h1>
+                    </Link>
+                </div>
+                <button
+                    onClick={handleDarkModeToggle}
+                    aria-label="Toggle dark mode"
+                    className='focus:outline-none'
+                >
+                    {
+                        darkMode ? (
+                            <MdWbSunny className='text-2xl cursor-pointer' />
+                        ) : (
+                            <MdNightsStay className='text-2xl cursor-pointer' />
+                        )
+                    }
+                </button>
+            </nav>
+        </header>
+    )
 }
 
 Header.propTypes = {
