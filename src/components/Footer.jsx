de966337e96c98b68aa585dc10b3ef1dc73c57b1
Cris <ghinghiloschicristina@gmail.com>
2023-08-30 11:03:27 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 export const Footer = () => {
     return (
-      <section className="text-center py-2 text-xs text-rose-700">
+      <section className="text-center py-2 text-xs text-rose-700 dark:text-white">
           <p className="">
               Created by Cristina Ghinghiloschi © 2023
           </p>
