a291c5ffb62c35b15ff8cf82e65f3712cb7bdfce
Cris <ghinghiloschicristina@gmail.com>
2023-08-30 09:21:05 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
     <div className={darkMode ? 'dark' : ''}>
       <Router>
         <div className="h-screen">
-          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
           <main className="bg-gradient-to-b from-rose-700 via-rose-500 to-amber-200 dark:from-slate-950 dark:via-slate-800 dark:to-cyan-800 text-white">
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/timer" element={<Timer />} />
