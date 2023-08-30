9a54eab2ef80af20db74be15f8da1142fc477965
Cris <ghinghiloschicristina@gmail.com>
2023-08-30 09:16:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <div className={`${darkMode ? 'dark' : ''} overflow-y-hidden`}>
+    <div className={darkMode ? 'dark' : ''}>
       <Router>
         <div className="h-screen">
           <Header darkMode={darkMode} setDarkMode={setDarkMode} />
