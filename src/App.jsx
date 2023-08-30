794a5e783afb2fccee228990e6032655578464d7
Cris <ghinghiloschicristina@gmail.com>
2023-08-30 09:39:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className={darkMode ? 'dark' : ''}>
       <Router>
-        <div className="h-screen">
+        <div className="">
           <main className="bg-gradient-to-b from-rose-700 via-rose-500 to-amber-200 dark:from-slate-950 dark:via-slate-800 dark:to-cyan-800 text-white">
           <Header darkMode={darkMode} setDarkMode={setDarkMode} />
             <Routes>
